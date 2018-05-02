7dbf6d35cf9b0b40b6bb5664827d105e2b05e8f6
suryaprakashs <SuryaprakashSivaraj@JANDBINDIA.COM>
2018-05-02 14:55:13 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# windows-notifier
-A simple app that creates toasts in windows notification center
+﻿# Creating a Toast notifier for windows
+
+This shiz took me 4 hours to complete with a lot of struggle and a lot of Stackoverflow Driven Development
+
+Oh, I build the app to use command line args for the notification text and header.
+
+## The steps I did to make it work
+
+### Step 1
+Create a new project and add unload the project. Then edit the project settings add the following line to the property group
+```
+<TargetPlatformVersion>8.0</TargetPlatformVersion> 
+```
+and another reference to the project
+```
+<Reference Include="System.Runtime" /><Reference 
+```
+### Step 2:
+We need windows reference for the application to work. Lets add them too. 
+Note: windows reference only will show after editing the project for TargetPlatformVersion to 8.0.
+
+Add the following reference by choosing 'Add Reference' context menu on the project.
+```
+Windows.UI
+Windows.Data
+```
+### Step 3:
+After that it is critical that we need create a application ID for our application to work. ie, to make notification to the system api.
+Without this application id we won't able to make API requests to make toast. Please refer: [Toast not working after windows update](https://stackoverflow.com/questions/46814858/toast-notification-not-working-on-windows-fall-creators-update)
+
+For that 
+Install the following nuget package inorder to access the System shell to create the application
+```
+WindowsAPICodePack-Shell
+```
+### Step 4:
+Other logics are too simple to write it here. Go look the code.
+Compile the app and run. It should probably work. :)
+
+	
---
+++ SimpleMan.Notifier/Program.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using MS.WindowsAPICodePack.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace SimpleMan.Notifier
+{
+    class Program
+    {
+        private const string _applicationId = "SimpleMan.Notifier.Toaster";
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> options = new Dictionary<string, string>()
+            {
+                { "ApplicationId", _applicationId },
+                { "Text", "No text!" },
+                { "Header", "Notification!" }
+            };
+
+            // set notification text
+            string text = GetOptionValue(args, "-t");
+            if (!String.IsNullOrEmpty(text))
+                options["Text"] = text;
+
+            // set notification header
+            string header = GetOptionValue(args, "-h");
+            if (!String.IsNullOrEmpty(text))
+                options["Header"] = header;
+
+            SimpleShortcut simepleShortcut = new SimpleShortcut();
+
+            simepleShortcut.TryCreateShortcut(_applicationId);
+
+            Notify notify = new Notify();
+            notify.NotifyText(options);
+
+        }
+
+        static string GetOptionValue(string[] args, string option)
+        {
+            if (args.Count() == 0 || !args.Contains(option))
+            {
+                return null;
+            }
+
+            int indexOfTextValue = Array.IndexOf(args, option) + 1;
+
+            if (indexOfTextValue >= args.Count())
+                return null;
+            else
+                return args[indexOfTextValue];
+        }
+    }
+
+
+    class Notify
+    {
+        public void NotifyText(Dictionary<string, string> options)
+        {
+            // Get a toast XML template
+            // Can be any template type. REF: https://docs.microsoft.com/en-us/uwp/api/windows.ui.notifications.toasttemplatetype
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+
+            // Fill in the text elements
+            XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
+
+            // Header
+            stringElements[0].AppendChild(toastXml.CreateTextNode(options["Header"]));
+
+            // Notification text
+            stringElements[1].AppendChild(toastXml.CreateTextNode(options["Text"]));
+
+            // Specify the absolute path to an image; 
+            // FOR IF YOU ARE USING A IMAGE TOAST TEMPLATE
+            //String imagePath = "file:///" + Path.GetFullPath("toastImageAndText.png");
+            //XmlNodeList imageElements = toastXml.GetElementsByTagName("image");
+            //imageElements[0].Attributes.GetNamedItem("src").NodeValue = imagePath;
+
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            // Show the toast. Be sure to specify the AppUserModelId on your application's shortcut!
+            ToastNotificationManager.CreateToastNotifier(options["ApplicationId"]).Show(toast);
+
+        }
+
+    }
+
+    class SimpleShortcut
+    {
+        public bool TryCreateShortcut(string appId)
+        {
+            String shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Windows\\Start Menu\\Programs\\SimpleMan Toast Notifier.lnk";
+            if (!File.Exists(shortcutPath))
+            {
+                InstallShortcut(shortcutPath, appId);
+                return true;
+            }
+            return false;
+        }
+
+        private void InstallShortcut(String shortcutPath, string applicationId)
+        {
+            // Find the path to the current executable 
+            String exePath = Process.GetCurrentProcess().MainModule.FileName;
+            IShellLinkW newShortcut = (IShellLinkW)new CShellLink();
+
+            // Create a shortcut to the exe 
+            ErrorHelper.VerifySucceeded(newShortcut.SetPath(exePath));
+            ErrorHelper.VerifySucceeded(newShortcut.SetArguments(""));
+
+            // Open the shortcut property store, set the AppUserModelId property 
+            IPropertyStore newShortcutProperties = (IPropertyStore)newShortcut;
+
+            using (PropVariant appId = new PropVariant(applicationId))
+            {
+                ErrorHelper.VerifySucceeded(newShortcutProperties.SetValue(SystemProperties.System.AppUserModel.ID, appId));
+                ErrorHelper.VerifySucceeded(newShortcutProperties.Commit());
+            }
+
+            // Commit the shortcut to disk 
+            IPersistFile newShortcutSave = (IPersistFile)newShortcut;
+
+            ErrorHelper.VerifySucceeded(newShortcutSave.Save(shortcutPath, true));
+        }
+    }
+}
